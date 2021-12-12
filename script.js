66c63d353b89bb45aca21da2b70e77ed0d1bdd35
HuuDuc <duc11102000@gmail.com>
2021-12-12 15:49:47 +0700

    add sound button

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const btn = document.getElementById("search-btn");
 const result = document.getElementById("result");
+const sound = document.getElementById("sound");
 const api = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+function playSound() {
+    sound.play();
+}
 
 btn.addEventListener('click', () => {
     const input  = document.getElementById("input").value;
@@ -11,8 +16,10 @@ btn.addEventListener('click', () => {
         console.log(data);
         result.innerHTML = `
         <h3>${input}</h3>
+        <button onclick="playSound()"></button>
         <p>${data[0].meanings[0].definitions[0].definition}</p>
         <p>${data[0].meanings[0].definitions[0].example}</p>`
+        sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`)
      })
      .catch(() => {
         result.innerHTML = '?';
